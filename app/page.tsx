7366f8a1c3e82a06d17f96944912591b68dc8b37
okmic <okmic.g@gmail.com>
2026-05-22 21:51:57 +0300

    loading page

--- app/page.tsx
+++ app/page.tsx
@@ -11,7 +11,7 @@ export default function Home() {
   const [isLoading, setIsLoading] = useState(true);
 
   if(isLoading) {
-        <Loading onComplete={() => setIsLoading(false)} />
+      return <Loading onComplete={() => setIsLoading(false)} />
   }
 
   return (
